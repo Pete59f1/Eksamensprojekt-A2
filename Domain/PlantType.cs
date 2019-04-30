6101b5079e134763fd8172c5166666a8c96eb558
Michal Tomasz Odzienkowski <mich040m@edu.eal.dk>
2019-04-30 14:21:27 +0200

    Implementering af PlantType klassen og enumet PictureStatus

---
+++ Domain/PlantType.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class PlantType
+    {
+
+        public int PlantNumber { get; private set; }
+        public string Type { get; private set; }
+        public int ExpectedGrowthTimeInDays { get; private set; }
+
+        public PlantType(string type, int expectedGrowthTimeInDays, int plantNumber)
+        {
+            Type = type;
+            ExpectedGrowthTimeInDays = expectedGrowthTimeInDays;
+            PlantNumber = plantNumber;
+        }
+
+
+    }
+}
