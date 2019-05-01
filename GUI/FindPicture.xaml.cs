0c207050228c73038e3e5f750ee07b0ef2746903
Peter Starup <pete59f1@edu.eal.dk>
2019-05-01 13:20:26 +0200

    Er gået i gang med FindPicture GUI

---
+++ GUI/FindPicture.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace GUI
+{
+    /// <summary>
+    /// Interaction logic for FindPicture.xaml
+    /// </summary>
+    public partial class FindPicture : Window
+    {
+        public FindPicture()
+        {
+            InitializeComponent();
+        }
+
+        private void Btn_Back_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow main = new MainWindow();
+            main.Show();
+            this.Close();
+        }
+    }
+}
