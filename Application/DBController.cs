fcc9960bb981b1e28a98fe135ffbc7fe86e648d1
Niclas Holmquist Jensen <nicl2445@edu.eal.dk>
2019-05-08 11:53:25 +0200

    db con string

--- Application/DBController.cs
+++ Application/DBController.cs
@@ -8,7 +8,7 @@ namespace GruppeA2.Application
 {
     abstract class DBController
     {
-        private string connectionString = "Bla";
+        private string connectionString = "SERVER = ealSQL1.eal.local; Database = A_DB28_2018; User Id = A_STUDENT28; Password = A_OPENDB28;";
 
         public string ConnectionString { get { return connectionString; } private set { connectionString = value; } }
     }
