87f4781a7179c92b3583f897da25ba0fcd66a521
Peter Starup <pete59f1@edu.eal.dk>
2019-05-18 14:41:03 +0200

    Har arbejdet på noget test

--- Test/DomainTest.cs
+++ Test/DomainTest.cs
@@ -12,6 +12,23 @@ namespace Test
     [TestClass]
     public class DomainTest
     {
+        static DateTime date;
+        Picture pictureTest;
 
+        [TestInitialize]
+        public void DomainTestInitialize()
+        {
+            date = new DateTime(2019 - 01 - 01, 11, 32);
+            pictureTest = new Picture(1, date, "HEJ", "1", "C:\\Test");
+        }
+
+        
+        //Picture Test
+        [TestMethod]
+        public void TestPictureFindStatus()
+        {
+            PictureStatus stat = pictureTest.FindStatus("2");
+            Assert.AreEqual(PictureStatus.NormalGrowth, stat);
+        }
     }
 }
