e0ee07b2744a9c559c4aca613527acd4ff493d09
Peter Starup <pete59f1@edu.eal.dk>
2019-05-20 14:08:01 +0200

    Lavet en ændring i start batch

--- GUI/StartBatch.xaml.cs
+++ GUI/StartBatch.xaml.cs
@@ -75,7 +75,10 @@ namespace GUI
         private void Btn_start_batch_Click(object sender, RoutedEventArgs e)
         {
             string chosen_type = cb_PlantType.SelectedItem.ToString();
+            string chosen_phase = cb_Phase.SelectedItem.ToString().Substring(8, 2);
+            int phase = cb_Phase.SelectedIndex;
             int plantId = 0;
+            
             for (int i = 0; i < plants.Count; i++)
             {
                 if (chosen_type == plants.get_plant_type(plants.GetItem(i)))
@@ -83,12 +86,11 @@ namespace GUI
                     plantId = plants.get_plant_number(plants.GetItem(i));
                 }
             }
-            string phase = cb_Phase.SelectedItem.ToString().Substring(8, 2);
-            int fase = int.Parse(phase);
-            DateTime start = DateTime.Now;
-            DateTime end = DateTime.Now.Date.AddDays(int.Parse(phase));
 
-            con.new_batch(fase, start, end, plantId);
+            DateTime start = DateTime.Now;
+            DateTime end = DateTime.Now.Date.AddDays(phase);
+
+            con.new_batch(phase, start, end, plantId);
         }
     }
 }
