266ec95fd66809f44cf7fe56e58adbd4cf7f49a9
Peter Starup <pete59f1@edu.eal.dk>
2019-05-21 15:52:22 +0200

    Arbejdet videre på new_batch metode..Muligvis gjort den færdig

--- Application/AddDBController.cs
+++ Application/AddDBController.cs
@@ -17,6 +17,7 @@ namespace GruppeA2.Application
             {
                 try
                 {
+                    //Create new batch
                     con.Open();
 
                     SqlCommand cmd = new SqlCommand("sp_NewBatch", con);
@@ -26,6 +27,10 @@ namespace GruppeA2.Application
                     cmd.Parameters.Add(new SqlParameter("@endDate", end_date));
                     cmd.Parameters.Add(new SqlParameter("@plantId", plantId));
                     cmd.ExecuteNonQuery();
+
+
+                    //Get the new batch's number
+                    int batchNr = 0;
 
                     cmd = new SqlCommand("sp_GetNewBatchId");
                     cmd.CommandType = CommandType.StoredProcedure;
@@ -37,18 +42,23 @@ namespace GruppeA2.Application
                         while (read.Read())
                         {
                             string batId = read["BatchNr"].ToString();
-                            int batchNr = int.Parse(batId);
+                            batchNr = int.Parse(batId);
                         }
                     }
 
+
+                    //Create the number of days
                     int days = ((TimeSpan)(end_date - start_date)).Days;
 
                     cmd = new SqlCommand("sp_CreateDay");
                     cmd.CommandType = CommandType.StoredProcedure;
 
-                    for (int i = 1; i <= days; i++)
+                    for (int i = 0; i < days; i++)
                     {
-
+                        cmd.Parameters.Add(new SqlParameter("@DayNr", i + 1));
+                        cmd.Parameters.Add(new SqlParameter("@Date", start_date.AddDays(i)));
+                        cmd.Parameters.Add(new SqlParameter("@BatchNr", batchNr));
+                        cmd.ExecuteNonQuery();
                     }
 
                 }
